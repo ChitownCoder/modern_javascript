6ff675c129a4bb862b84cb9d4c5c0afaab4b4f39
ChitownCoder <chitowncoder@gmail.com>
2022-02-26 00:26:13 -0600

    added a delete funciton

--- Section_06/51_Book_List/app.js
+++ Section_06/51_Book_List/app.js
@@ -47,6 +47,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 };
 
+// Delete book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById("title").value = "";
@@ -54,7 +61,7 @@ UI.prototype.clearFields = function () {
   document.getElementById("isbn").value = "";
 };
 
-// Event Listeners
+// Event Listeners for add book
 document.getElementById("book-form").addEventListener(
   "submit",
   // Get form values
@@ -87,3 +94,17 @@ document.getElementById("book-form").addEventListener(
     e.preventDefault();
   }
 );
+
+// Event listener for delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show alert
+  ui.showAlert("Book Removed!", "success");
+
+  e.preventDefault();
+});
